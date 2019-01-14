e1f06831fdff90846837b958e58b8c290b523cc3
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2019-01-14 19:30:32 +1000

    production preparations

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env nodejs
+
 /* eslint no-sync: ["error", { allowAtRootLevel: true }] */
 
 const fs = require('fs')
@@ -7,8 +9,9 @@ const localeCode = fs.readFileSync('./node_modules/intl/locale-data/jsonp/ru.js'
 
 const content = `${polyfillCode}\n${localeCode}\n`
 
+// https://github.com/3rd-Eden/useragent/blob/master/lib/regexps.js
 const browsers = [
-  /XiaoMi\/MiuiBrowser/u,
+  /XiaoMi\/MiuiBrowser/,
 ]
 
 const polyfillRequired = (userAgent) => browsers.some((browser) => browser.test(userAgent))
